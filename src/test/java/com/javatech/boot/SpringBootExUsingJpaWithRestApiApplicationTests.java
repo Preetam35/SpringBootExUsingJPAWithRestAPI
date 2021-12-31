90b40a39df4613ec93cbebd4d5f7e1a0f1800c0a
Sonu Kumar Sah <java.sonukumarsah@gmail.com>
2021-12-31 11:48:04 +0530

    First Commit

---
+++ src/test/java/com/javatech/boot/SpringBootExUsingJpaWithRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatech.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootExUsingJpaWithRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
